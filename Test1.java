ab2e84883bf4ed7127c2fc7415242ed3e6c7bee8
unknown <Bollu Harika>
2026-05-06 20:19:17 +0530

    Added conditional greeting logic

--- Test1.java
+++ Test1.java
@@ -1,5 +1,11 @@
-public class Test1{
-public static void main(String[] args){
-System.out.println("Hello,Harika!!!");
-}
+public class Test1 {
+    public static void main(String[] args) {
+        String name = "Harika";
+
+        if (name.equals("Harika")) {
+            System.out.println("Welcome back, Harika!");
+        } else {
+            System.out.println("Hello, user!");
+        }
+    }
 }
